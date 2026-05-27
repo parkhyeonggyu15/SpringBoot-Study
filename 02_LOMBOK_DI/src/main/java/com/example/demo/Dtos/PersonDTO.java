49b49d2dc1e668a3f4354d6a9633a3ba0dfccfee
parkhyeonggyu15 <gudrb15@naver.com>
2026-05-27 17:33:03 +0900

    01DAYS-SPRINGBOOT

---
+++ 02_LOMBOK_DI/src/main/java/com/example/demo/Dtos/PersonDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo.Dtos;
+
+// LOMBOK 확인 : CTRL +F12
+// JUNIT TEST CASE : CTRL + SHIFT + T
+
+import lombok.Data;
+
+// 확인 CTRL + F12
+//@Getter
+//@Setter
+//@ToString
+@Data
+public class PersonDTO {
+    private String name;
+    private int age;
+    private String addr;
+}
