5a20b6ca1b5412a22e52a5c327888f3f3c25b61c
parkhyeonggyu15 <gudrb15@naver.com>
2026-06-01 17:52:50 +0900

    04DAYS-SPRINGBOOT

---
+++ 08_SQLMAPPER_MYBATIS/src/test/java/com/example/demo/Config/MybatisConfigTest.java
@@ -0,0 +1,21 @@
+package com.example.demo.Config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MybatisConfigTest {
+
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    public void t1(){
+        assertNotNull(sqlSessionFactory);
+
+    }
+}
